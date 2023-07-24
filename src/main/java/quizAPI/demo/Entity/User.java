4e2f6131d87d586a81931e08f54068960717eea9
unknown <keit.lancine@odc.local>
2023-07-24 18:59:31 +0000

    version 1.1.2

--- src/main/java/quizAPI/demo/Entity/User.java
+++ src/main/java/quizAPI/demo/Entity/User.java
@@ -1,2 +1,32 @@
-package quizAPI.demo.Entity;public class User {
+package quizAPI.demo.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String nom;
+    @Column(nullable = false , length = 100)
+    private String prenom;
+    @Column(nullable = false)
+    private int age;
+    @Column(nullable = false, length = 225)
+    private String telephone;
+
+    @OneToMany(mappedBy = "utilisateur", cascade = CascadeType.ALL)
+    @Column(nullable = false)
+    @JsonIgnoreProperties("utilisateur")
+    private List<Quiz> quiz;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String motDePass;
 }
--- src/main/java/quizAPI/demo/Repository/User_repository.java
+++ src/main/java/quizAPI/demo/Repository/User_repository.java
@@ -1,2 +1,15 @@
-package quizAPI.demo.Repository;public interface User_repository {
+package quizAPI.demo.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+import quizAPI.demo.Entity.User;
+
+import java.util.List;
+
+public interface User_repository  extends CrudRepository <User, Long> {
+    User creer(User user);
+    List<User> Lire();
+    User getUser(Long id, User user);
+
+    User modifier(Long id, User user);
+    String supprimer(Long id);
 }
