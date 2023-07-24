17f1e58346fa20e8eaefa2c38851aa008bc089e6
Ousmato <ousmatotoure98@gmail.com>
2023-07-24 17:34:47 +0000

    version 1.0

---
+++ src/test/java/quizAPI/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package quizAPI.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
